2647d00ff365a1039c1a2b6c4bd571ae6b31f1fd
zhyoulun <zhyoulun@gmail.com>
2019-08-24 14:10:05 +0800

    add chapter 03

---
+++ chapter-03/codes/fork.c
@@ -0,0 +1,20 @@
+#include<sys/types.h>
+#include<stdio.h>
+#include<unistd.h>
+
+
+int main(){
+    pid_t pid;
+    pid = fork();
+    if (pid<0){
+        fprintf(stderr, "fork failed\n");
+        return 1;
+    }
+    else if(pid==0){
+        execlp("/bin/ls", "ls", NULL);
+    }else{
+        wait(NULL);
+        printf("child complete\n");
+    }
+    return 0;
+}
